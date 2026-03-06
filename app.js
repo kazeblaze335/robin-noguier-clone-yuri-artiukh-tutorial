92fa1d0271f1adaf1280fcdea19be63ee917086d
johnmilner <johnmilner>
2026-03-06 10:51:45 -0800

    second commit - line animation

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 let speed = 0;
 let position = 0;
+let rounded = 0;
 let block = document.querySelector('#block');
+let elems = [...document.querySelectorAll('.n')];
 
 window.addEventListener('wheel', (e) => {
-    speed += e.deltaY * 0.00062;
+    speed += e.deltaY * 0.0003;
 })
 
+let objs = Array(5).fill({dist: 0})
+
 function raf() {
-    console.log(position);
     position += speed;
     speed *= 0.8;
-    block.style.transform = `translate(0, ${position*100}px)`;
+
+    objs.forEach((o, i) => {
+        o.dist = (position - i)/4;
+        o.dist = Math.abs(o.dist);
+        elems[i].style.transform = `scale(${1 - o.dist})`;
+    
+    })
+    rounded = Math.round(position);
+
+    let diff = (rounded - position);
+
+    position += Math.sign(diff)*Math.pow(Math.abs(diff), 0.7)*0.015;
+    console.log(position, "======")
+    block.style.transform = `translate(0, ${position*100 + 50}px)`;
     window.requestAnimationFrame(raf);
 }
 
